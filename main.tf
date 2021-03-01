b47ff067626041782baeb3eb27cb8f1332c2b349
jhp0204 <78405325+jhp0204@users.noreply.github.com>
2021-03-01 17:22:28 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,12 +7,6 @@ terraform{
   }
 }
 
-provider "aws" {
-  access_key="AKIASOQOFZRYZSYM6X5O"
-  secret_key="K3M+TZwKphSpmeI9SK2lphJvZEYCs6HqpzcTlC9W"
-  region  = "ap-northeast-2"
-}
-
 resource "aws_instance" "example" {
   ami           = "ami-09282971cf2faa4c9"
   instance_type = "t2.micro"
