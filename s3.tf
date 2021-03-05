4b8821a8b4251e576eaf95b50403c36f9a7ac764
jhp0204 <78405325+jhp0204@users.noreply.github.com>
2021-03-05 13:32:16 +0900

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,10 +1,10 @@
 resource "aws_s3_bucket" "terraform-state" {
   bucket = "terraform-up-and-running-state"
 
-  versioning = {
+  versioning {
     enabled = true
   }
-  lifecycle = {
+  lifecycle {
     prevent_destroy = true
   }
 }
