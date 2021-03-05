644e0081560de6645467f257f9838244bb82c1f9
jhp0204 <78405325+jhp0204@users.noreply.github.com>
2021-03-05 14:15:54 +0900

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -8,3 +8,12 @@ resource "aws_s3_bucket" "jhp0204qkrwngns2" {
     prevent_destroy = true
   }
 }
+
+terraform {
+  backend "s3" {
+    bucket = "erraform-up-and-running-state-jhp0204"
+    key = "terraform.tfstate"
+    region = "us-east-2"
+    encrypt = true
+  }
+}
