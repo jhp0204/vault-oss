6b78e676d7fda3280af09b976feae4b315146f47
jhp0204 <78405325+jhp0204@users.noreply.github.com>
2021-03-09 12:04:15 +0900

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "example" {
   ami           = "ami-09246ddb00c7c4fef"
   instance_type = "t2.micro"
-  vpc_security_group_ids = ["${aws_security_group.example-allow-all.id}"]
+  vpc_security_group_ids = ["${aws_security_group.main-allow-all.id}"]
   tags = {
     Name = "demo-instance"
     Purpose = "Terraform Demo"
