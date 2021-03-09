5b433074ea0a9976be69bc63c89cc573eca718f4
jhp0204 <78405325+jhp0204@users.noreply.github.com>
2021-03-09 11:05:17 +0900

    Update subnet.tf

--- subnet.tf
+++ subnet.tf
@@ -1,5 +1,5 @@
 resource "aws_subnet" "side_effect_public_subnet1" {
-  vpc_id = "${aws_vpc.side_effect.id}"
+  vpc_id = "${aws_vpc.main.id}"
   cidr_block = "10.10.1.0/16"
   map_public_ip_on_launch = false
   tags = {
