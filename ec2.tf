317748aa7118030559909b465b60404b3f801b00
jhp0204 <78405325+jhp0204@users.noreply.github.com>
2021-03-09 12:09:53 +0900

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "example" {
   ami           = "ami-09246ddb00c7c4fef"
   instance_type = "t2.micro"
+  subnet_id = "${aws_subnet.main_public_subnet1.id}"
   vpc_security_group_ids = ["${aws_security_group.main-allow-all.id}"]
   tags = {
     Name = "demo-instance"
