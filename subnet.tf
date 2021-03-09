a6fbd3fccdfb4aacf10ca5c70244495d836e7332
jhp0204 <78405325+jhp0204@users.noreply.github.com>
2021-03-09 11:06:51 +0900

    Update subnet.tf

--- subnet.tf
+++ subnet.tf
@@ -1,4 +1,4 @@
-resource "aws_subnet" "side_effect_public_subnet1" {
+resource "aws_subnet" "main_public_subnet1" {
   vpc_id = "${aws_vpc.main.id}"
   cidr_block = "10.10.1.0/16"
   map_public_ip_on_launch = false
@@ -6,3 +6,14 @@ resource "aws_subnet" "side_effect_public_subnet1" {
     Name = "demo-subnet"
   }
 }
+
+resource "aws_subnet" "main_public_subnet2" {
+  vpc_id = "${aws_vpc.main.id}"
+  cidr_block = "10.10.2.0/16"
+  map_public_ip_on_launch = false
+  tags = {
+    Name = "demo-subnet2"
+  }
+}
+
+# map_public_ip_on_launch : vpc 내에서 server를 띄울때 자동으로 public ip가 할당된다. 
