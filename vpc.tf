f779d76cfd28fcac432562fa0afdbac9d47f5474
jhp0204 <78405325+jhp0204@users.noreply.github.com>
2021-03-09 18:22:02 +0900

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -8,11 +8,3 @@ resource "aws_vpc" "main" {
 }
 
 
-resource "aws_default_route_table" "main" {
-  default_route_table_id = "${aws_vpc.main.default_route_table_id}"
-
-  tags {
-    Name = "demo-route-table"
-  }
-} 
-# vpc 생성 시, 자동으로 생기는 route table을 Terraform에서 관리하기 위하여 argument값만 가져온다. 
