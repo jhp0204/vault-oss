283c5dd8e9d5837842df44226fcd072e4b180338
jhp0204 <78405325+jhp0204@users.noreply.github.com>
2021-03-09 11:04:40 +0900

    Create subnet.tf

---
+++ subnet.tf
@@ -0,0 +1,8 @@
+resource "aws_subnet" "side_effect_public_subnet1" {
+  vpc_id = "${aws_vpc.side_effect.id}"
+  cidr_block = "10.10.1.0/16"
+  map_public_ip_on_launch = false
+  tags = {
+    Name = "demo-subnet"
+  }
+}
