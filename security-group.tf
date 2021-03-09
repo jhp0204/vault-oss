ff14e71a074b2c65800fcf1cf4b201986f5551a5
jhp0204 <78405325+jhp0204@users.noreply.github.com>
2021-03-09 11:59:45 +0900

    Create security-group.tf

---
+++ security-group.tf
@@ -0,0 +1,19 @@
+resource "aws_security_group" "main-allow-all" {
+  name = "main-allow-all"
+  description = "Allow all inbound traffic"
+  vpc_id = "${aws_vpc.main.id}"
+
+  ingress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
