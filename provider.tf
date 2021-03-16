dcca3ed3e0b1345ef7a132030fb15a2e03c9cb49
jhp0204 <78405325+jhp0204@users.noreply.github.com>
2021-03-16 17:43:09 +0900

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region  = "${var.region}"
-  access_key = "${var.access_key}"
-  secret_key = "${var.secret_key}"
+  access_key = "${var.access_key_id}"
+  secret_key = "${var.secret_key_id}"
 }
