02e140f6431619c444524c427c70687bed6ebeb2
jhp0204 <78405325+jhp0204@users.noreply.github.com>
2021-03-16 18:29:49 +0900

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,5 +1,10 @@
 variable "region" {
+  description = "AWS region"
   default = "us-east-2"
 }
-variable "access_key_id" {}
-variable "secret_key_id" {}
+variable "access_key_id" {
+  access_key_id = "AKIASOQOFZRYQKTPYDKP"
+  }
+variable "secret_key_id" {
+  secret_key_id = "hg7ZABdAWcJcUBFGMzGVS1wPG1XItGxNftB0zJIy"
+  }
