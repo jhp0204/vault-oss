cac91ae558a5925bcc556a508943c24cb40b8347
jhp0204 <78405325+jhp0204@users.noreply.github.com>
2021-04-08 17:43:40 +0900

    Create ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "example" {
   ami           = "ami-09246ddb00c7c4fef"
   instance_type = "t2.micro"
-  key_name = "tfe_test"
+  key_name = "aws_kc"
   tags = {
     Name = "demo-ansible-instance"
     Purpose = "Terraform Ansible Connection test"
