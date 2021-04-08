fcd2f0e5671701e83c7e36324caa3404a4e49498
jhp0204 <78405325+jhp0204@users.noreply.github.com>
2021-04-08 17:00:27 +0900

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "example" {
   instance_type = "t2.micro"
   subnet_id = var.subnet_id
   vpc_security_group_ids = [aws_security_group.allow_22_all.id]
-  key_name = "terraform-keypair"
+  key_name = var.ec2_key
   tags = {
     Name = "demo-ansible-instance"
     Purpose = "Terraform Ansible Connection test"
