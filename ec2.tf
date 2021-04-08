cb2273957c864a56449dbf167915b33e3ed38123
jhp0204 <78405325+jhp0204@users.noreply.github.com>
2021-04-08 15:08:07 +0900

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -6,6 +6,32 @@ resource "aws_instance" "example" {
   key_name = "terraform-keypair"
   tags = {
     Name = "demo-instance"
-    Purpose = "Terraform Demo"
+    Purpose = "Terraform Ansible Connection test"
+  }
+  connection {
+    user = "ubuntu"
+    type = "ssh"
+
+    private_key = file("~/.ssh/authorized_keys") # 해당 부분 수정이 필요한지 check 실제 폴더에 있는 파일은 authorized_keys이지만, pem파일명은 terraform-keypair.pem 이다. 
+    timeout     = "3m"
+  }
+  
+  provisioner "remote-exec" {
+    inline = [
+      "sudo yum update",
+      "sudo yum install -y python",
+    ]
+  }
+  provisioner "local-exec" {
+    command = <<EOF
+      echo "[demo]" > inventory
+      echo "${aws_instance.example.public_ip} ansible_ssh_user=ubuntu ansible_ssh_private_key_file=~/.ssh/your_private_key.pem" >> inventory
+      EOF
+  }
+  provisioner "local-exec" {
+    command = <<EOF
+      ANSIBLE_HOST_KEY_CHECKING=False \
+      ansible-playbook -i inventory playbook.yml
+      EOF
   }
 }
