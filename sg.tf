64bb694e43cbdc1ce28c6b9b80d2bf02c8cb4f28
jhp0204 <78405325+jhp0204@users.noreply.github.com>
2021-04-08 13:59:46 +0900

    Update sg.tf

--- sg.tf
+++ sg.tf
@@ -1,7 +1,7 @@
 resource "aws_security_group" "allow_22_all" {
   name = "allow_22_all"
   description = "allow ssh port from all"
-  vpc_id = module.vpc.vpc_id_output
+  vpc_id = module.vpc.vpc_id
   ingress {
     cidr_blocks = [ "0.0.0.0/0" ]
     from_port = 22
