6f5a58d4109547d60628d3164d71266f84f6d54b
jhp0204 <78405325+jhp0204@users.noreply.github.com>
2021-04-08 13:52:18 +0900

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -6,7 +6,3 @@ module "vpc" {
   source = "github.com/jhp0204/terraform-aws-simple/vpc/"
 }
 
-module "ec2sg" {
-  source = "github.com/jhp0204/terraform-aws-simple/"
-  vpc_id = module.vpc.vpc_id
-}
