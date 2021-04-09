6127c1845afc56dfde9ac473c7aa11b3aeb2833c
jhp0204 <78405325+jhp0204@users.noreply.github.com>
2021-04-09 13:25:41 +0900

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region  = "us-east-2"
-#   access_key = var.AWS_ACCESS_KEY_ID
-#   secret_key = var.AWS_SECRET_ACCESS_KEY
+  access_key = var.AWS_ACCESS_KEY_ID
+  secret_key = var.AWS_SECRET_ACCESS_KEY
 }
 
 module "vpc" {
