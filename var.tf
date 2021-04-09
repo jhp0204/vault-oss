a9d17193cfd18cbeb59ecbf7996cf5181b08823d
jhp0204 <78405325+jhp0204@users.noreply.github.com>
2021-04-09 13:09:35 +0900

    Update var.tf

--- var.tf
+++ var.tf
@@ -1,5 +1,5 @@
 variable "region" {}
 variable "ec2_key" {}
-variable "AWS_ACCESS_KEY_ID" {}
-variable "AWS_SECRET_ACCESS_KEY" {}
+# variable "AWS_ACCESS_KEY_ID" {}
+# variable "AWS_SECRET_ACCESS_KEY" {}
 
