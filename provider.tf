2bd424bb89877b77090ea3db29b2802e8d753954
jhp0204 <78405325+jhp0204@users.noreply.github.com>
2021-04-13 16:54:04 +0900

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,10 +1,11 @@
 provider "aws" {
-  region  = "us-east-2"
+  region  = module.vpc.data_json
   access_key = var.AWS_ACCESS_KEY_ID
   secret_key = var.AWS_SECRET_ACCESS_KEY
 }
 
 module "vpc" {
-  source = "github.com/jhp0204/terraform-aws-simple/vpc/"
+  source = "github.com/jhp0204/vault-oss/vpc"
 }
+
 
