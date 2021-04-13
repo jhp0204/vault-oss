033ffca05ba69f3bf4455aab92d7c7f47a729a83
jhp0204 <78405325+jhp0204@users.noreply.github.com>
2021-04-13 21:03:22 +0900

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -3,10 +3,7 @@ provider "aws" {
   access_key = var.AWS_ACCESS_KEY_ID
   secret_key = var.AWS_SECRET_ACCESS_KEY
 }
-provider "vault" {
-    address = "http://127.0.0.1:8200"
-    token = "s.ojO2vDeAlngypg2aJZKkzI56"
-}
+
 module "vpc" {
   source = "github.com/jhp0204/vault-oss/vpc"
 }
